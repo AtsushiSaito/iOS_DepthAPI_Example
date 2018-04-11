329f9ae1482f1e7148f5b029158cdcbb19286a94
AtsushiSaito <atsushi31100854@gmail.com>
2018-04-12 00:34:33 +0900

    First Commit

--- SwiftDepth/AppDelegate.swift
+++ SwiftDepth/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    private(set) lazy var viewController = ViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = viewController
         return true
     }
 
--- SwiftDepth/ViewController.swift
+++ SwiftDepth/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var HomeLabel: UILabel!
 
+    var DeviceInput: AVCaptureDeviceInput!
+    var PhotoOutput: AVCapturePhotoOutput!
+    var CaptureSession: AVCaptureSession!
+    var Camera: AVCaptureDevice!
+    var PreView: UIView!
+    
+    private var photoData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.HomeLabel = UILabel()
+        self.HomeLabel.text = "iOS11 DepthAPI TestApp"
+        self.HomeLabel.frame = CGRectMake(0, 30, self.view.bounds.width, 20)
+        self.HomeLabel.textAlignment = .center
+        self.view.addSubview(self.HomeLabel)
+        
+        self.PreView = UIView()
+        self.PreView.frame = CGRectMake(0, 60, self.view.bounds.width, self.view.bounds.height - 60)
+        
+        self.InitCamera()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func InitCamera(){
+        self.CaptureSession = AVCaptureSession()
+        self.Camera = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: .video, position: .back)
+        
+        do {
+            self.DeviceInput = try AVCaptureDeviceInput(device: self.Camera)
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        if (self.CaptureSession.canAddInput(self.DeviceInput)) {
+            self.CaptureSession.addInput(DeviceInput)
+        }
+        
+        let PreviewLayer = AVCaptureVideoPreviewLayer(session: self.CaptureSession)
+        
+        PreviewLayer.frame = PreView.frame
+        PreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        self.view.layer.addSublayer(PreviewLayer)
+        
+        self.CaptureSession.startRunning()
+    }
+    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
 
 }
 
