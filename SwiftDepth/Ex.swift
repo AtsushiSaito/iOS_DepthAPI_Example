dbbd3cc9d6e6d4e70ba919a269c0488fcda00ab4
AtsushiSaito <atsushi31100854@gmail.com>
2018-04-16 01:36:29 +0900

    Add Depth View(Test)

---
+++ SwiftDepth/Ex.swift
@@ -0,0 +1,45 @@
+//
+//  Ex.swift
+//  SwiftDepth
+//
+//  Created by AtsushiSaito on 2018/04/12.
+//  Copyright © 2018年 AtsushiSaito. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+extension CVPixelBuffer {
+    
+    func normalize() {
+        
+        let width = CVPixelBufferGetWidth(self)
+        let height = CVPixelBufferGetHeight(self)
+        
+        CVPixelBufferLockBaseAddress(self, CVPixelBufferLockFlags(rawValue: 0))
+        let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(self), to: UnsafeMutablePointer<Float>.self)
+        
+        var minPixel: Float = 1.0
+        var maxPixel: Float = 0.0
+        
+        for y in 0 ..< height {
+            for x in 0 ..< width {
+                let pixel = floatBuffer[y * width + x]
+                minPixel = min(pixel, minPixel)
+                maxPixel = max(pixel, maxPixel)
+            }
+        }
+        
+        let range = maxPixel - minPixel
+        
+        for y in 0 ..< height {
+            for x in 0 ..< width {
+                let pixel = floatBuffer[y * width + x]
+                floatBuffer[y * width + x] = (pixel - minPixel) / range
+            }
+        }
+        
+        CVPixelBufferUnlockBaseAddress(self, CVPixelBufferLockFlags(rawValue: 0))
+    }
+}
--- SwiftDepth/ViewController.swift
+++ SwiftDepth/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 import AVFoundation
 
+extension ViewController: AVCaptureDataOutputSynchronizerDelegate {
+    func dataOutputSynchronizer(_ synchronizer: AVCaptureDataOutputSynchronizer, didOutput synchronizedDataCollection: AVCaptureSynchronizedDataCollection) {
+        let DepthSyncData = synchronizedDataCollection.synchronizedData(for: self.DepthOutput) as? AVCaptureSynchronizedDepthData
+        let DepthData = DepthSyncData?.depthData
+        
+        if (DepthData != nil){
+            let DepthImage = CIImage(cvPixelBuffer: (DepthData?.applyingExifOrientation(.right).depthDataMap)!)
+            DispatchQueue.main.async {
+                self.DepthImageView.image = UIImage(ciImage: DepthImage)
+            }
+        }
+        
+        let VideoSyncData = synchronizedDataCollection.synchronizedData(for: self.VideoOutput) as? AVCaptureSynchronizedSampleBufferData
+        let SampleBuffer = VideoSyncData?.sampleBuffer
+        if (VideoSyncData?.sampleBufferWasDropped == false) {
+            DispatchQueue.main.async {
+                let Image = self.UIImageFromCMSamleBuffer(buffer: SampleBuffer!)
+                self.VideoImageView.image = Image
+            }
+        } else {
+            print("true")
+        }
+    }
+}
+
 class ViewController: UIViewController {
     
     var HomeLabel: UILabel!
 
     var DeviceInput: AVCaptureDeviceInput!
+    
     var PhotoOutput: AVCapturePhotoOutput!
+    var DepthOutput: AVCaptureDepthDataOutput!
+    var VideoOutput: AVCaptureVideoDataOutput!
+    var SyncOutput: AVCaptureDataOutputSynchronizer!
+    
     var CaptureSession: AVCaptureSession!
     var Camera: AVCaptureDevice!
-    var PreView: UIView!
     
-    private var photoData: Data?
+    var DepthImageView: UIImageView!
+    var VideoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,8 +61,14 @@ class ViewController: UIViewController {
         self.HomeLabel.textAlignment = .center
         self.view.addSubview(self.HomeLabel)
         
-        self.PreView = UIView()
-        self.PreView.frame = CGRectMake(0, 60, self.view.bounds.width, self.view.bounds.height - 60)
+        self.VideoImageView = UIImageView()
+        self.VideoImageView.frame = CGRectMake(0, 60, self.view.bounds.width, self.view.bounds.height - 60)
+        self.view.addSubview(self.VideoImageView)
+        
+        self.DepthImageView = UIImageView()
+        self.DepthImageView.frame = CGRectMake(0, 60, self.view.bounds.width, self.view.bounds.height - 60)
+        self.DepthImageView.alpha = 1.0
+        self.view.addSubview(self.DepthImageView)
         
         self.InitCamera()
     }
@@ -42,8 +78,11 @@ class ViewController: UIViewController {
     }
     
     func InitCamera(){
-        self.CaptureSession = AVCaptureSession()
-        self.Camera = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: .video, position: .back)
+        
+        if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            self.Camera = dualCameraDevice
+            print("DualCamera")
+        }
         
         do {
             self.DeviceInput = try AVCaptureDeviceInput(device: self.Camera)
@@ -51,23 +90,73 @@ class ViewController: UIViewController {
             print(error)
         }
         
+        self.CaptureSession = AVCaptureSession()
+        self.CaptureSession.sessionPreset = .photo
+        
+        self.CaptureSession.beginConfiguration()
+        
         if (self.CaptureSession.canAddInput(self.DeviceInput)) {
             self.CaptureSession.addInput(DeviceInput)
         }
         
-        let PreviewLayer = AVCaptureVideoPreviewLayer(session: self.CaptureSession)
+        let DepthOutputQueue = DispatchQueue(label: "DepthData Queue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
         
-        PreviewLayer.frame = PreView.frame
-        PreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.DepthOutput = AVCaptureDepthDataOutput()
+        self.PhotoOutput = AVCapturePhotoOutput()
+        self.VideoOutput = AVCaptureVideoDataOutput()
         
-        self.view.layer.addSublayer(PreviewLayer)
+        if (self.CaptureSession.canAddOutput(self.PhotoOutput)) {
+            self.CaptureSession.addOutput(self.PhotoOutput)
+        }
+        
+        if (self.PhotoOutput.isDepthDataDeliverySupported) {
+            self.PhotoOutput.isDepthDataDeliveryEnabled = true
+            self.DepthOutput.isFilteringEnabled = true
+        }
+        
+        if (self.CaptureSession.canAddOutput(self.DepthOutput)) {
+            self.CaptureSession.addOutput(self.DepthOutput)
+            if let connection = self.DepthOutput.connection(with: .depthData) {
+                connection.isEnabled = true
+            } else {
+                print("No AVCaptureConnection")
+            }
+        }
+        
+        if (self.CaptureSession.canAddOutput(self.VideoOutput)) {
+            self.CaptureSession.addOutput(self.VideoOutput)
+        }
+        
+        self.SyncOutput = AVCaptureDataOutputSynchronizer(dataOutputs: [self.VideoOutput, self.DepthOutput])
+        self.CaptureSession.commitConfiguration()
+        
+        self.SyncOutput.setDelegate(self, queue: DepthOutputQueue)
         
         self.CaptureSession.startRunning()
+
     }
     
     func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
         return CGRect(x: x, y: y, width: width, height: height)
     }
-
+    
+    func UIImageFromCMSamleBuffer(buffer: CMSampleBuffer)-> UIImage {
+        // サンプルバッファからピクセルバッファを取り出す
+        let pixelBuffer:CVImageBuffer = CMSampleBufferGetImageBuffer(buffer)!
+        
+        // ピクセルバッファをベースにCoreImageのCIImageオブジェクトを作成
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        
+        //CIImageからCGImageを作成
+        let pixelBufferWidth = CGFloat(CVPixelBufferGetWidth(pixelBuffer))
+        let pixelBufferHeight = CGFloat(CVPixelBufferGetHeight(pixelBuffer))
+        let imageRect:CGRect = CGRectMake(0,0,pixelBufferWidth, pixelBufferHeight)
+        let ciContext = CIContext.init()
+        let cgimage = ciContext.createCGImage(ciImage, from: imageRect )
+        
+        // CGImageからUIImageを作成
+        let image = UIImage(cgImage: cgimage!, scale: 1.0, orientation: UIImageOrientation.right)
+        return image
+    }
 }
 
